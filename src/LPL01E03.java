4243946e982e1bd33e0e8c28dab2736423d2a528
Douglas Coimbra <143850668+DCLaass@users.noreply.github.com>
2024-09-12 09:11:34 -0300

    Update LPL01E03.java
    Leia um valor e faça um programa que coloque o valor lido na primeira posição de um vetor de 10 posições. Em cada posição subsequente, coloque o dobro do valor da posição anterior. Por exemplo, se o valor lido for 1, os valores do vetor devem ser 1, 2, 4, 8 e assim sucessivamente. Mostre o vetor em seguida.

--- src/LPL01E03.java
+++ src/LPL01E03.java
@@ -1,3 +1,4 @@
+//Leia um valor e faça um programa que coloque o valor lido na primeira posição de um vetor de 10 posições. Em cada posição subsequente, coloque o dobro do valor da posição anterior. Por exemplo, se o valor lido for 1, os valores do vetor devem ser 1, 2, 4, 8 e assim sucessivamente. Mostre o vetor em seguida.
 import java.util.Scanner;
 
 public class LPL01E03 {
