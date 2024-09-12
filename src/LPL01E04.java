5ea48390c2b06a6ea5233aee2cca8802c6710a4a
Douglas Coimbra <143850668+DCLaass@users.noreply.github.com>
2024-09-12 09:11:58 -0300

    Update LPL01E04.java
    Leia um valor X. Coloque este valor na primeira posição de um vetor N de 100 posições. Em cada posição subsequente de N (1 até 99), coloque a metade do valor armazenado na posição anterior, conforme o exemplo abaixo. Imprima o vetor N.

--- src/LPL01E04.java
+++ src/LPL01E04.java
@@ -1,3 +1,4 @@
+//Leia um valor X. Coloque este valor na primeira posição de um vetor N de 100 posições. Em cada posição subsequente de N (1 até 99), coloque a metade do valor armazenado na posição anterior, conforme o exemplo abaixo. Imprima o vetor N.
 import java.util.Scanner;
 
 public class LPL01E04 {
