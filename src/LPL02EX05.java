53eea331ddb2262777a6b781d8ef8e11f39be3e5
Douglas Coimbra <143850668+DCLaass@users.noreply.github.com>
2024-10-06 10:30:27 -0300

    Update and rename L02EX05.java to LPL02EX05.java

--- src/LPL02EX05.java
+++ src/LPL02EX05.java
@@ -1,8 +1,8 @@
 //Implemente uma função recursiva que calcule a soma de todos os elementos de um array.
 import java.util.Scanner;
-public class L02EX05 {
+public class LPL02EX05 {
     public static int somaVetor(int[]vetor, int tamanho){
-        if (tamanho == vetor.length) {
+        if (tamanho < 0) {
             return 0;
         }
         return vetor[tamanho] + somaVetor(vetor, tamanho + 1);
@@ -13,9 +13,9 @@ public class L02EX05 {
         int tamanho = scanner.nextInt();
         int[] vetor = new int[tamanho];
         System.out.println("Digite os elementos do seu Array: ");
-        for (int i = 0; i < tamanho; i++) {
-            vetor[i] = scanner.nextInt();
-        }
+            for (int i = 0; i < tamanho; i++) {
+                vetor[i] = scanner.nextInt();
+            }
         int maior = somaVetor(vetor, 0);
         System.out.println("A soma dos elementos do Array é: "+ maior);
     }
