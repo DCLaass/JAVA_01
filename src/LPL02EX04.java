30712c2846f4b69f85849f54bbf66b33ff0bf4db
Douglas Coimbra <143850668+DCLaass@users.noreply.github.com>
2024-10-06 10:29:35 -0300

    Update and rename L02EX04.java to LPL02EX04.java

--- src/LPL02EX04.java
+++ src/LPL02EX04.java
@@ -1,6 +1,6 @@
 //Escreva uma função recursiva que encontre o maior elemento em um array de inteiros.
 import java.util.Scanner;
-public class L02EX04 {
+public class LPL02EX04 {
     public static int maiorInteiro(int[]vetor, int tamanho, int maior){
         if (tamanho == vetor.length) {
             return maior;
@@ -16,9 +16,10 @@ public class L02EX04 {
         int tamanho = scanner.nextInt();
         int[] vetor = new int[tamanho];
         System.out.println("Digite os elementos do seu Array:");
-        for (int i = 0; i < tamanho; i++) {
-            vetor[i] = scanner.nextInt();
-        }
+            for (int i = 0; i < tamanho; i++) {
+                vetor[i] = scanner.nextInt();
+            }
+        
         int maior = maiorInteiro(vetor, 0,vetor[0]);
         System.out.println("O maior elemento do seu Array é: "+ maior);
     }
