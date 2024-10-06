b402ca06cc447aa04c460e510dd9fa51db4285e0
Douglas Coimbra <143850668+DCLaass@users.noreply.github.com>
2024-10-06 10:24:16 -0300

    Update and rename LPL01E03.java to LPL01EX03.java

--- src/LPL01EX03.java
+++ src/LPL01EX03.java
@@ -1,7 +1,7 @@
 //Leia um valor e faça um programa que coloque o valor lido na primeira posição de um vetor de 10 posições. Em cada posição subsequente, coloque o dobro do valor da posição anterior. Por exemplo, se o valor lido for 1, os valores do vetor devem ser 1, 2, 4, 8 e assim sucessivamente. Mostre o vetor em seguida.
 import java.util.Scanner;
 
-public class LPL01E03 {
+public class LPL01EX03 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int[] VET01 = new int[10];
