b5c5bd3dce0bafa7f2cde7d704f9cd256ea67f2f
Douglas Coimbra <143850668+DCLaass@users.noreply.github.com>
2024-10-06 10:32:00 -0300

    Update and rename L02EX07.java to LPL02EX07.java

--- src/LPL02EX07.java
+++ src/LPL02EX07.java
@@ -1,6 +1,6 @@
 //Implemente uma função recursiva para calcular o Máximo Divisor Comum (MDC) de dois números usando o algoritmo de Euclides.
 import java.util.Scanner;
-public class L02EX07 {
+public class LPL02EX07 {
     public static int calcularMDC(int a, int b){
         if(b==0){
             return a;
@@ -13,6 +13,7 @@ public class L02EX07 {
         int a = scanner.nextInt();
         System.out.println("Digite o outro valor para descobrir o Máximo Divisor Comum (MDC), b: ");
         int b = scanner.nextInt();
+        
         int resultado = calcularMDC(a,b);
         System.out.println("O o Máximo Divisor Comum (MDC), de a: "+a+", e b: "+b+", é igual a: "+resultado);
     }
